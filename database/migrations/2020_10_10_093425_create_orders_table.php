29876d704ada89e3c6b2691c49d04eccdd1d951e
Darius <1357280829@qq.com>
2020-10-14 14:30:33 +0800

    修补

--- database/migrations/2020_10_10_093425_create_orders_table.php
+++ database/migrations/2020_10_10_093425_create_orders_table.php
@@ -15,13 +15,15 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('no')->comment('订单编号');
+            $table->string('taking_code')->comment('自取号');
             $table->string('phone')->comment('联系电话');
-            $table->timestamp('arrived_at')->comment('自提时间');
+            $table->string('arrived_time')->comment('自提时间');
             $table->json('products')->comment('商品快照');
-            $table->decimal('packing_price', 10, 2)->default(0.00)->comment('包装费');
-            $table->decimal('delivery_price', 10, 2)->default(0.00)->comment('配送费');
             $table->decimal('total_price', 10, 2)->default(0.00)->comment('合计价');
+            $table->unsignedTinyInteger('is_paid')->default(0)->comment('是否支付');
             $table->text('remark')->nullable()->comment('备注');
+            $table->unsignedBigInteger('wechat_user_id')->comment('微信用户id');
             $table->timestamps();
             $table->softDeletes();
         });
---
+++ database/migrations/2020_10_13_173556_create_authenticateds_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthenticatedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //  认证者表
+        Schema::create('authenticateds', function (Blueprint $table) {
+            $table->id();
+            $table->string('authenticate_type')->comment('认证类型;mini_program:小程序认证,official_account:公众号认证');
+            $table->string('authenticated_user_model')->comment('认证者用户模型;wechat:微信用户模型');
+            $table->unsignedBigInteger('authenticated_user_id')->comment('认证者用户id');
+            $table->string('token')->comment('token');
+            $table->unsignedInteger('ttl')->default(0)->comment('生命周期(单位:秒)，0表示无限');
+            $table->timestamp('expired_at')->nullable()->comment('失效时间，空表示无限');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authenticateds');
+    }
+}
